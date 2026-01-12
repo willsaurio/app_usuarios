ca5e17dec70bde958fa462f4c4cbf0eddffb43f9
willsaurio <wmazeledonado8a@gmail.com>
2026-01-12 11:54:28 -0600

    se agrego componente Button

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
 import { useEffect } from 'react';
-import { StyleSheet, Text, View, Button, ActivityIndicator, TextInput, Alert, TouchableOpacity } from 'react-native';
-import { COLORS } from '@core';
+import { StyleSheet, View } from 'react-native';
+import { Button } from '@core';
 
 export default function App() {
   const [loaded, error] = useFonts({
@@ -23,20 +23,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Button title="Presioname!" onPress={() => Alert.alert('ya fui al baño uwu')}></Button>
-      <TouchableOpacity
-        style={{
-          backgroundColor: COLORS.primary,
-          padding: 16,
-          borderRadius:8,
-          marginTop: 20
-        }
-        }
-        onPress={() => Alert.alert('Hola mundo')}>
-        <Text style={{ color: COLORS.white, fontSize:24 }}>Presioname tambien!</Text>
-      </TouchableOpacity>
-      <ActivityIndicator size="large" color="green" />
-      <TextInput style={{backgroundColor:'pink', width:300, padding:16}} />
+      <Button title="Hello" onPress={() => console.log('Hello')} />
     </View>
   )
 }
---
+++ src/core/components/Button.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, TouchableOpacity, } from 'react-native'
+import React from 'react'
+import { COLORS } from '@core'
+
+interface ButtonProps {
+    title: string;
+    onPress: () => void;
+ }
+
+export const Button = ({title, onPress}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+            style={styles.btn}
+            onPress={onPress}>
+          <Text style={styles.txt}>{title}</Text>
+          </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: COLORS.primary,
+    padding: 16,
+    borderRadius: 8,
+    marginTop: 20,
+  },
+  txt: {
+    color: COLORS.white,
+    fontSize: 24,
+  },
+})
